04d5cef3e26d4245fbb3b66eb6d8ecbf3ff21ae1
Ubuntu <ubuntu@ip-172-31-41-7.ap-south-1.compute.internal>
2024-09-26 15:08:49 +0000

    s3

---
+++ modules/s3/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "modules3" {
+    bucket = "modules3"
+    acl = "private"
+    tags = {
+        Name = "modules3"
+    }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_s3" {
+    bucket = aws_s3_bucket.modules3.bucket
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
